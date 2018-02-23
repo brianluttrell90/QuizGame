3a2cb16fa1ab19308b25aa312ea7ab6b0ff55a67
brianluttrell90 <brianluttrell90@gmail.com>
2018-02-23 09:18:56 -0600

    Add files via upload

---
+++ AnswerData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class AnswerData 
+{
+    public string answerText;
+    public bool isCorrect;
+    
+}
---
+++ MenuScreenController.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class MenuScreenController : MonoBehaviour {
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
